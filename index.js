70b1c03566ea09385547dd56b4d23e191b6a70a2
Danielle Mizrachi <dmiz@brandeis.edu>
2019-01-08 13:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-let clone= object.assign ({}, object);
+let clone= Object.assign ({}, object);
  delete clone[key];
   return clone; 
 }
