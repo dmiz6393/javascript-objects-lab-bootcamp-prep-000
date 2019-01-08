bc0c6e29a6002f043d8a43f34691cab0e0731d99
Danielle Mizrachi <dmiz@brandeis.edu>
2019-01-08 12:41:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 
 const recipes= {};
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]=value; 
+  return object;
+}
